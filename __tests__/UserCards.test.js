b6bae32a7c1dceb54c9a4c04385e94b90728eed4
jubayer1431 <mdhridoy625@gmail.com>
2025-01-01 23:28:03 +0600

    removed babel + jest config

--- __tests__/UserCards.test.js
+++ __tests__/UserCards.test.js
@@ -1,4 +1,8 @@
-import { fetchUsers } from '../lib/fetchUsers';
+async function fetchUsers() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  if (!res.ok) throw new Error('Failed to fetch users');
+  return res.json();
+}
 describe('UserCards', () => {
   it('should fetch exactly 10 users', async () => {
     const users = await fetchUsers();
