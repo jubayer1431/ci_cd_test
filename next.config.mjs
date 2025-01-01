27005fe838d7bfdf28c25da33e21324b3f18233a
jubayer1431 <mdhridoy625@gmail.com>
2025-01-01 14:37:48 +0600

    GitHub Pages deployment workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+// next.config.js
+export default {
+  output: 'export', // Enables static exporting
+  basePath: '/ci_cd_test', // Replace with your repository name
+};
